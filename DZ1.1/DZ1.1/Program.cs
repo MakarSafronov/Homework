ade9e24217ba05fbe48a25ff9d2c2ec09e8987f0
MakarSafronov <makarsafronov1@gmail.com>
2024-11-27 10:30:14 +0300

    Первые две домашки

---
+++ DZ1.1/DZ1.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class PrimeNumbers
+{
+    public static void print_primes(int n)
+    {
+        if (n<2)
+        {
+            Console.WriteLine("простых чисел нет");
+            return;
+        }
+        bool[] isPrime = new bool[n+1];
+        for (int i = 2; i<=n; i++)
+        {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i*i<=n; i++)
+        {
+            if (isPrime[i])
+            {
+                for(int j = i*i; j<=n; j+=i)
+                { 
+                    isPrime[j] = false; 
+                } 
+            }
+        }
+        Console.WriteLine($"простые числа,не превосходяшие {n}:");
+        for (int i = 2; i<=n; i++)
+        {
+            if (isPrime[i])
+            {
+                Console.Write(i+" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("введите число: ");
+        int n=int.Parse(Console.ReadLine());
+        PrimeNumbers.print_primes(n);
+    }
+}
---
+++ DZ1.3/DZ1.3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+public class EquationSolver
+{
+    public static void SolveEquation(double a,double b, double c)
+    {
+        if (a==0)
+        {
+            if (b == 0)
+            {
+                if (c==0)
+                {
+                    Console.WriteLine("уравнение имеет бесконечно много решений");
+                }
+                else
+                {
+                    Console.WriteLine("уравнение не имеет решений");
+                }
+            }
+            else
+            {
+                double x = -c/b;
+                Console.WriteLine($"перед нами линейное уравнение и его решение: x={x}");
+            }
+        }
+        else
+        {
+            double D = b*b-4*a*c;
+            if (D>0)
+            {
+                double x1 = (-b+Math.Sqrt(D))/(2*a);
+                double x2 = (-b-Math.Sqrt(D))/(2*a);
+                Console.WriteLine($"уравнение имеет два решения: x1={x1}, x2={x2}");
+            }
+            else if (D==0)
+            {
+                double x = (-b)/(2*a);
+                Console.WriteLine($"уравнение имеет одно решение: x={x}");
+            }
+            else
+            {
+                Console.WriteLine($"уравнение не имеет действительных решений");
+            }
+        }
+
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("введите коэффициенты уравнения (a,b,c): ");
+        Console.Write("a=");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("b=");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("c=");
+        double c = double.Parse(Console.ReadLine());
+        EquationSolver.SolveEquation(a, b, c);
+    }
+}

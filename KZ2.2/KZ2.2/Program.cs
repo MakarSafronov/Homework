822a741ea4628343280d55bbc832fb69be344ab7
MakarSafronov <makarsafronov1@gmail.com>
2024-11-29 11:28:02 +0300

    Следующие 3 задания

---
+++ KZ2.2/KZ2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[10];
+        Console.WriteLine("Введите десять различных чисел: ");
+        for (int i = 0; i < 10; i++)
+        { 
+            Console.Write($"число {i+1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        int max = numbers[0];
+        int secondmax = int.MinValue;
+        for (int i = 0; i < 10; i++)
+        {
+            if (numbers[i] > max)
+            {
+                secondmax = max;
+                max = numbers[i];
+            }
+            else if (numbers[i] > secondmax)
+            {
+                secondmax= numbers[i];
+            }
+        }
+        Console.WriteLine($"Наибольшее число: {max}");
+        Console.WriteLine($"Следующее по величине число: {secondmax}");
+    }
+}
---
+++ KZ2.3/KZ2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Opera
+{
+    public static int OperaSteps(int n)
+    {
+        int steps = 0;
+        while (n != 1)
+        {
+            if (n%2==0)
+            {
+                n/=2;
+            }
+            else
+            {
+                n=3*n+1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите число n: ");
+        int n=int.Parse(Console.ReadLine());
+        int steps=Opera.OperaSteps(n);
+        Console.WriteLine($"Количество замен для числа {n}, приводящих к 1: {steps}");
+    }
+}
